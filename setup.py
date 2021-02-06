46186d4802076866c396f4bf600ef316cf6f874d
Kamil Pawlaczyk <prgmr.kamil.pawlaczyk@gmail.com>
2021-02-06 13:29:49 +0100

    chorge: official pre-alpha version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except IOError:
 
 setup(
     name="github-views-bot",
-    version="0.0.1",
+    version="0.0.2",
     description="bot to increase the number of views on github",
     license="GNU",
     author="Kamil Pawlaczyk",
