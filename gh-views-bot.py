622e6bb3ff08ed9493e2b75312d73ed63496aed5
Milesq <wmilosz88@gmail.com>
2021-02-06 10:39:50 +0100

    refactor: add default params

--- gh-views-bot.py
+++ gh-views-bot.py
@@ -13,14 +13,14 @@ def request_task(url, data, headers):
     requests.get(url, json=data, headers=headers)
 
 
-def fire_and_forget(url, json, headers):
+def fire_and_forget(url, json={}, headers={}):
     threading.Thread(target=request_task, args=(url, json, headers)).start()
 
 
 def increse_views(url, num, timeout):
     print()
     for i in range(num):
-        fire_and_forget(url,{},{})
+        fire_and_forget(url)
         sys.stdout.flush()
         print(f"{CURSOR_UP_ONE}{ERASE_LINE}Added: {i+1}/{num}")
         time.sleep(timeout/1000)
@@ -30,7 +30,7 @@ def increse_views_infinite(url, timeout):
     print()
     i = 0
     while True:
-        fire_and_forget(url,{},{})
+        fire_and_forget(url)
         sys.stdout.flush()
         print(f"{CURSOR_UP_ONE}{ERASE_LINE}Added: {i+1}/...")
         time.sleep(timeout/1000)
