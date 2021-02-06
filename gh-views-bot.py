080207b87565d811ec2329aeb1715aa460975327
Milesq <wmilosz88@gmail.com>
2021-02-06 10:28:40 +0100

    chore: add two blank lines between functions
    https://www.python.org/dev/peps/pep-0008/#blank-lines

--- gh-views-bot.py
+++ gh-views-bot.py
@@ -6,10 +6,15 @@ import requests
 CURSOR_UP_ONE = '\x1b[1A'
 ERASE_LINE = '\x1b[2K'
 
+
 def request_task(url, data, headers):
     requests.get(url, json=data, headers=headers)
+
+
 def fire_and_forget(url, json, headers):
     threading.Thread(target=request_task, args=(url, json, headers)).start()
+
+
 def increse_views(url, num, timeout):
     print()
     for i in range(num):
@@ -17,6 +22,8 @@ def increse_views(url, num, timeout):
         sys.stdout.flush()
         print(f"{CURSOR_UP_ONE}{ERASE_LINE}Added: {i+1}/{num}")
         time.sleep(timeout/1000)
+
+
 def increse_views_infinite(url, timeout):
     print()
     i = 0
@@ -26,6 +33,7 @@ def increse_views_infinite(url, timeout):
         print(f"{CURSOR_UP_ONE}{ERASE_LINE}Added: {i+1}/...")
         time.sleep(timeout/1000)
         i += 1
+
 
 url = 'https://camo.githubusercontent.com/' #address of views
 
@@ -40,7 +48,7 @@ except:
 
 print("views increaser:")
 try:
-    if "--infinite" in sys.argv:    
+    if "--infinite" in sys.argv:
         increse_views_infinite(url, timeout)
     else:
         increse_views(url, num, timeout)
